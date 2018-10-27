02c9f5a2b2e4a86d1ef873d9f516996994803ad7
MBP 2017 <38024526+NitramDivad@users.noreply.github.com>
2018-10-27 11:47:23 -0600

    gen updates

--- api/routes/index.js
+++ api/routes/index.js
@@ -19,18 +19,18 @@ router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
 
 // leaderboard display
-router.get('/leaderboard', function (req, res) {
-  console.log('I received a GET request');
-  user.find({ "user":"score" }, function(err, users) {
-    if(!err){
-      res.send(users);
-    }
-    else{
-        res.send('could not retrived data');
-  }
-  });
+// router.get('/leaderboard', function (req, res) {
+//   console.log('I received a GET request');
+//   user.find({ "user":"score" }, function(err, users) {
+//     if(!err){
+//       res.send(users);
+//     }
+//     else{
+//         res.send('could not retrived data');
+//   }
+//   });
 
-});
+// });
 
 // // get user messages
 // router.get('/messages', function (req, res) {
--- dist/mean-angular6/main.js
+++ dist/mean-angular6/main.js
@@ -1587,7 +1587,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/david/Documents/BootcampProjects/dcrypt-template/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/david/Documents/BootcampProjects/DCrypt/src/main.ts */"./src/main.ts");
 
 
 /***/ })
